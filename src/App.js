e8ab57a096117dec7d90322bc7236deab142d990
MMMonir <helpinfosdesk@gmail.com>
2021-10-14 12:00:52 +0600

    useParams inside Booking.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Booking from './Pages/Booking/Booking';
 import Home from './Pages/Home/Home/Home';
 import NotFound from './Pages/Home/NotFound/NotFound';
 
@@ -14,6 +15,9 @@ function App() {
           </Route>
           <Route path="/home">
             <Home></Home>
+          </Route>
+          <Route path="/booking/:serviceId">
+            <Booking></Booking>
           </Route>
           <Route path="*">
             <NotFound></NotFound>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Service.css';
 
 const Service = (props) => {
-    const {name, img, price, description} = props.service;
+    const {id, name, img, price, description} = props.service;
     return (
-        <div className="service">
+        <div className="service pb-3">
             <img src={img} alt="" style={{width: "100%"}}/>
             <h3>{name}</h3>
             <p>Price: {price}</p>
             <p className="px-3">{description}</p>
+            <Link to={`/booking/${id}`}><button className="btn btn-warning">Book {name.toLowerCase()}</button></Link>
         </div>
     );
 };
